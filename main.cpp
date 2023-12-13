c0c6a66d97416f35741117f11f138a5fccf9ab1a
harveylwo <my751721909@gmail.com>
2023-12-13 22:22:06 +0800

    assignment4 completed, including the bonus objective

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
+#include <algorithm>
 #include <chrono>
+#include <cmath>
 #include <iostream>
+#include <memory>
+#include <opencv2/core/matx.hpp>
 #include <opencv2/core/types.hpp>
 #include <opencv2/opencv.hpp>
 
@@ -57,6 +61,11 @@ cv::Point2f recursive_bezier(const std::vector<cv::Point2f> &control_points, flo
 
 }
 
+template<typename T>
+T lerp(T a, T b, float t) {
+    return (1-t)*a + t*b;
+}
+
 void bezier(const std::vector<cv::Point2f> &control_points, cv::Mat &window) 
 {
     // TODO: Iterate through all t = 0 to t = 1 with small steps, and call de Casteljau's 
@@ -64,7 +73,36 @@ void bezier(const std::vector<cv::Point2f> &control_points, cv::Mat &window)
     for (double t = 0.0; t <= 1.0; t += 0.0001) 
     {
         auto point = recursive_bezier(control_points, t);
-        window.at<cv::Vec3b>(point.y, point.x)[1] = 255;
+        // implement the anti-aliasing of the curve
+        // window.at<cv::Vec3b>(point.y, point.x)[1] = 255;
+
+        // get topleft, topright, bottomleft, bottomright, the floor value should be compared with the height or width of the window
+        // depending on the location of the point inside a pixel(less or no less than 0.5), the ajacent pixels should be different
+        auto x = std::floor(point.x), y = std::floor(point.y);
+        auto dx = point.x - x, dy = point.y - y;
+        auto min_x = std::max(0.0f, dx<0.5f?x-1:x), max_x = std::min(window.cols-1.0f, dx<0.5f?x:x+1);
+        auto min_y = std::max(0.0f, dy<0.5f?y-1:y), max_y = std::min(window.rows-1.0f, dy<0.5f?y:y+1);
+        // auto min_x = x, max_x = std::ceil(x);
+        // auto min_y = y, max_y = std::ceil(y);
+        auto distance = std::numeric_limits<float>::max();
+        for(auto tx : {min_x, max_x}){
+            for(auto ty : {min_y, max_y}){
+                auto dtx = tx+0.5f - point.x, dty = ty+0.5f - point.y;
+                auto d = std::sqrt(dtx*dtx + dty*dty);
+                distance = std::min(distance, d);
+            }
+        }
+
+        // interpolate each pixel's value by lerp
+        for(auto tx : {min_x, max_x}){
+            for (auto ty : {min_y, max_y}){
+                auto dtx = tx+0.5f - point.x, dty = ty+0.5f - point.y;
+                auto d = std::sqrt(dtx*dtx + dty*dty);
+                auto t = distance/d;
+                // ! IMPORTANT: each pixel may be visited multiple times, thus we need to use max to get the max value
+                window.at<cv::Vec3b>(ty, tx)[1] = std::max(255*t, (float)window.at<cv::Vec3b>(ty, tx)[1]);
+            }
+        }
     }
 
 }
