ec41f1e47f471cafdd8cfa45338bd6be89becde3
harveylwo <my751721909@gmail.com>
2023-12-27 00:37:20 +0800

    minor change to the y cordinates calculation

--- Renderer.cpp
+++ Renderer.cpp
@@ -230,7 +230,8 @@ void Renderer::Render(const Scene& scene)
             // Also, don't forget to multiply both of them with the variable *scale*, and
             // x (horizontal) variable with the *imageAspectRatio*            
             x = 2 * ((i + 0.5) / (float)scene.width - 0.5) * scale * imageAspectRatio;
-            y = 2 * (1.0 - (j + 0.5) / (float)scene.height - 0.5) * scale;
+            // the image space origin is at the topleft corner, thus y is flipped
+            y = 2 * ( -(j + 0.5) / (float)scene.height + 0.5) * scale;
 
             Vector3f dir = Vector3f(x, y, -1).normalize();// Don't forget to normalize this direction!
             framebuffer[m++] = castRay(eye_pos, dir, scene, 0);
